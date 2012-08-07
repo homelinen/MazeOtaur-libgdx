a49fb55e7ddc3c877fdce593b4ffb0482315b5bb
Calum Gilchrist <calum@homelinen.org>
2012-08-08 00:23:00 +0100

    Added an AIManager and Some Pathfinding
    Implemented A* pathfinding between points on the map.

---
+++ src/uk/calumgilchrist/mazeotaur/ai/AIManager.java
@@ -0,0 +1,169 @@
+package uk.calumgilchrist.mazeotaur.ai;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import uk.calumgilchrist.mazeotaur.Enemy;
+import uk.calumgilchrist.mazeotaur.MazeGenerator;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+
+public class AIManager {
+
+	private List<Enemy> creatures;
+	
+	public AIManager() {
+		creatures = new LinkedList<Enemy>();
+	}
+	
+	/**
+	 * Add a creature to the management list
+	 * @param enemy
+	 */
+	public void addCreature(Enemy enemy) {
+		creatures.add(enemy);
+	}
+	
+	/**
+	 * Update positions of creature, etc
+	 */
+	public void update() {
+		for (Enemy creature: creatures) {
+			creature.moveNode();
+		}
+	}
+	
+	/**
+	 * Find a path to the point
+	 * @param The maze to do operations on
+	 * @param start beginning point
+	 * @param goal Final point to reacg
+	 * @return List of points to follow
+	 */
+	public List<Vector2> findPath(MazeGenerator maze, Vector2 start, Vector2 goal) {
+		
+		PriorityQueue<PathNode> open = new PriorityQueue<PathNode>();
+		LinkedList<PathNode> closed = new LinkedList<PathNode>();
+		
+		open.add(new PathNode(start, getPathCost(start, goal)));
+		
+		PathNode curNode;
+		Iterator<Vector2> neighbours;
+		Vector2 tempNeighbour = new Vector2();
+		
+		int curcost = 0;
+		PathNode tempNode = new PathNode();
+		boolean inOpen = false;
+		boolean inClosed = false;
+		
+		Gdx.app.log("Open List", open.toString());
+		
+		while (!open.element().getPoint().equals(goal)) {
+			
+			curNode = open.remove();
+			closed.add(curNode);
+			
+			neighbours = maze.getSurroundingPoints(curNode.getPoint());
+			
+			while (neighbours.hasNext()) {
+				curcost = curNode.getCost() + 1;
+				
+				tempNeighbour = neighbours.next();
+				
+				tempNode = getPointInNode(open, tempNeighbour); 
+				inOpen = tempNode != null;
+				if (inOpen && (tempNode.getCost() > curcost)) {
+					open.remove(tempNode);
+				}
+				
+				tempNode = getPointInNode(closed, tempNeighbour);
+				inClosed = tempNode != null;
+//				if (inClosed && tempNode.getCost() < curcost) {
+//					closed.remove(tempNode);
+//				}
+				
+				if (!inOpen && !inClosed) {
+					open.add(new PathNode(tempNeighbour, curcost, curNode));
+				}
+			}
+		}
+
+		
+		closed.add(open.element());
+		
+		//Free up some space, don't need anymore
+		open.clear();
+		
+		return findRoute(closed, start);
+	}
+	
+	/**
+	 * Slow O(n) search to see if a point is present in a nodeList
+	 * 
+	 * Returns the cost, or -1 if not in list
+	 * @param nodeList List of PathNodes to search
+	 * @param point Point of comparison
+	 * @return The cost of the point in the list
+	 */
+	public PathNode getPointInNode(Collection<PathNode> nodeList, Vector2 point) {
+		Iterator<PathNode> nodes = nodeList.iterator();
+		
+		PathNode tempNode;
+		while (nodes.hasNext()) {
+			tempNode = nodes.next();
+			if (tempNode.getPoint().equals(point)) {
+//				Gdx.app.log("Node", "" + tempNode);
+//				Gdx.app.log("Point", "" + point);
+				
+				return tempNode;
+			}
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Find the route by following the parents of the nodes in the List
+	 * @param nodeList List of nodes
+	 * @param start Where you bagan
+	 * @return A shortened list of the path needed to be taken
+	 */
+	public List<Vector2> findRoute(List<PathNode> nodeList, Vector2 start) {
+		LinkedList<Vector2> route = new LinkedList<Vector2>();
+		
+		PathNode goal = nodeList.get(nodeList.size() - 1);
+		route.add(goal.getPoint());
+		
+		PathNode tempNode = goal.getParent();
+		route.add(tempNode.getPoint());
+		
+		while (!tempNode.getPoint().equals(start) && tempNode != null) {
+			tempNode = tempNode.getParent();
+			route.addFirst(tempNode.getPoint());
+		}
+		
+		return route;
+	}
+	
+	/**
+	 * Find the cost between start and end
+	 * @param start Initial Point
+	 * @param end Goal Point
+	 * @return The sum of the distance to the point
+	 */
+	public int getPathCost(Vector2 start, Vector2 end) {
+		Vector2 sum = start.cpy().sub(end);
+		int cost = 0;
+		
+		cost = (int) (Math.abs(sum.x) + Math.abs(sum.y));
+		
+		return cost;
+	}
+}
