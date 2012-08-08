4a5d947175d4502b56576cbd547c869dac99da09
Calum Gilchrist <calum@homelinen.org>
2012-08-08 15:28:55 +0100

    Added AIManager Test to Maze Suite

--- src/uk/calumgilchrist/mazeotaur/tests/MazeSuite.java
+++ src/uk/calumgilchrist/mazeotaur/tests/MazeSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestPlayer.class, TestMaze.class, TestMazeGenerator.class })
+@SuiteClasses({ TestPlayer.class, TestMaze.class, TestMazeGenerator.class, TestAIManager.class })
 public class MazeSuite {
 
 	@Test
