99636ac7a7a396c3e2d3939f19773eacc82ba030
Calum Gilchrist <calum@homelinen.org>
2012-08-11 13:15:20 +0100

    Added Tests for Searching Along the Line of Sight

--- src/uk/calumgilchrist/mazeotaur/tests/EnemyTest.java
+++ src/uk/calumgilchrist/mazeotaur/tests/EnemyTest.java
@@ -2,12 +2,16 @@ package uk.calumgilchrist.mazeotaur.tests;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import com.badlogic.gdx.files.FileHandle;
+
 import uk.calumgilchrist.mazeotaur.Enemy;
+import uk.calumgilchrist.mazeotaur.MazeTemplate;
 import uk.calumgilchrist.mazeotaur.Vecter;
 
 public class EnemyTest {
@@ -19,7 +23,8 @@ public class EnemyTest {
 	 */
 	@Before
 	public void setUp() {
-		enemy = new Enemy(10, "Hector", new Vecter(0,0), 5);
+		
+		enemy = new Enemy(10, "Hector", new Vecter(0,0), 5, 10);
 		assertEquals("Path is 0", 0, enemy.getPath().size());
 		
 		points = new LinkedList<Vecter>();
@@ -43,5 +48,30 @@ public class EnemyTest {
 		assertTrue("Enemy is at start", new Vecter(0, 0).equals(enemy.getPosition()));
 		enemy.moveNode(0.5f);
 		assertFalse("Enemy is not at start", new Vecter(0, 0).equals(enemy.getPosition()));
+	}
+	
+	@Test
+	public void testIsPlayerInSight() {
+		Vecter playPos = new Vecter(9,0);
+		
+		MazeTemplate maze = new MazeTemplate(10, 10);
+		FileHandle file = new FileHandle(new File("assets/testmazes/sightTest.txt"));
+		maze.createMaze(file);
+		System.out.println(maze.printMaze());
+		
+		enemy.setPosition(new Vecter(0,0));
+		assertTrue("Player is in sight", enemy.isPlayerInSight(playPos, maze));
+		
+		enemy.setPosition(new Vecter(9,0));
+		playPos.set(9, 9);
+		assertTrue("Player is in sight", enemy.isPlayerInSight(playPos, maze));
+		
+		enemy.setPosition(new Vecter(0,9));
+		playPos.set(9,9);
+		assertFalse("Player is not in sight", enemy.isPlayerInSight(playPos, maze));
+		
+		enemy.setPosition(new Vecter(0,0));
+		playPos.set(0, 9);
+		assertFalse("Player is not in sight", enemy.isPlayerInSight(playPos, maze));
 	}
 }
